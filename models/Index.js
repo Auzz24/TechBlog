42e091e13b53d14f97d1476f076bf86735e4d984
Austin Miller <austin.miller1424@gmail.com>
2022-02-27 13:37:46 -0600

    User routes complete, Post routes started

---
+++ models/Index.js
@@ -0,0 +1,14 @@
+// const Post = require('./Post');
+const User = require('./User');
+const Post = require('./Posts');
+
+// User.hasMany(Post, {
+//     foreignKey: 'user_id'
+//   });
+
+//   Post.belongsTo(User, {
+//     foreignKey: 'user_id',
+//     onDelete: 'SET NULL'
+//   });
+
+  module.exports = { User, Post};
--- models/User.js
+++ models/User.js
@@ -2,12 +2,42 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // create our User model
-class User extends Model {}
+class User extends Model {
+
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+      }
+}
 
 // define table columns and configuration
 User.init(
   {
     // TABLE COLUMN DEFINITIONS GO HERE
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true
+        }
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [4]
+        }
+      }
   },
   {
     // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
